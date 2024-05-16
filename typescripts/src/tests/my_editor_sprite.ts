2b846b642566a459ab030bc43d11c7741294e0a6
ialex32x <lin_032@hotmail.com>
2024-05-16 10:15:40 +0800

    add a sprite2d test script which runs in editor mode

---
+++ typescripts/src/tests/my_editor_sprite.ts
@@ -0,0 +1,47 @@
+import { Engine, PackedStringArray, Sprite2D } from "godot";
+import { export_, tool_ } from "../jsb/jsb.core";
+
+// see example code in GDScript/C# at:
+// https://docs.godotengine.org/en/stable/tutorials/plugins/running_code_in_the_editor.html#doc-running-code-in-the-editor
+
+@tool_()
+export default class MyEditorSprite extends Sprite2D {
+
+    /**
+     * get/set property for `export` (both must be defined)
+     */
+    @export_(jsb.VariantType.TYPE_FLOAT)
+    get speed(): number { return this._speed; }
+    set speed(value: number) {
+        if (this._speed != value) {
+            this._speed = value;
+            this.update_configuration_warnings();
+        }
+    }
+
+    /**
+     * plain field for `export`
+     */
+    @export_(jsb.VariantType.TYPE_INT)
+    unused_int = 0;
+
+    private _clockwise = false;
+    private _speed = 0;
+
+    _ready() {
+        this._clockwise = Engine.is_editor_hint();
+    }
+
+    _process(delta: number) {
+        const step = Math.PI * delta * (this._clockwise ? this._speed : -this._speed);
+        this.set_rotation(this.get_rotation() + step);
+    }
+
+    _get_configuration_warnings() {
+        let warnings = new PackedStringArray();
+        if (this._speed >= -0.01 && this._speed < 0.01) {
+            warnings.append("speed is too low");
+        }
+        return warnings;
+    }
+}
