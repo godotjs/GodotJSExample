403d0001439484478c68a610ed3151ab9a5b2363
ialex32x <lin_032@hotmail.com>
2024-06-28 17:35:35 +0800

    feat: play a song with zzfxm

--- javascripts/audio_generator/generator_demo.js
+++ javascripts/audio_generator/generator_demo.js
@@ -8,81 +8,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 const godot_1 = require("godot");
 const jsb_core_1 = require("../jsb/jsb.core");
-const zzfxV = .3; // volume
-const zzfxR = 44100; // sample rate
-/**
- * ZzFX is a tiny sound generator designed to produce a wide variety of sound effects with minimal code overhead.
- * It's perfect for games, prototypes, and any web application that needs sound without the bulk of traditional sound files.
- *
- * @link https://killedbyapixel.github.io/ZzFX/
- * @link https://github.com/KilledByAPixel/ZzFX
- */
-class zzfx {
-    static sign(v) { return v > 0 ? 1 : -1; }
-    // generate samples
-    static generate(
-    // parameters
-    volume = 1, randomness = .05, frequency = 220, attack = 0, sustain = 0, release = .1, shape = 0, shapeCurve = 1, slide = 0, deltaSlide = 0, pitchJump = 0, pitchJumpTime = 0, repeatTime = 0, noise = 0, modulation = 0, bitCrush = 0, delay = 0, sustainVolume = 1, decay = 0, tremolo = 0) {
-        // init parameters
-        let PI2 = Math.PI * 2, startSlide = slide *= 500 * PI2 / zzfxR / zzfxR, startFrequency = frequency *= (1 + randomness * 2 * Math.random() - randomness) * PI2 / zzfxR, b = [], t = 0, tm = 0, i = 0, j = 1, r = 0, c = 0, s = 0, f, length;
-        // scale by sample rate
-        attack = attack * zzfxR + 9; // minimum attack to prevent pop
-        decay *= zzfxR;
-        sustain *= zzfxR;
-        release *= zzfxR;
-        delay *= zzfxR;
-        deltaSlide *= 500 * PI2 / zzfxR ** 3;
-        modulation *= PI2 / zzfxR;
-        pitchJump *= PI2 / zzfxR;
-        pitchJumpTime *= zzfxR;
-        repeatTime = repeatTime * zzfxR | 0;
-        // generate waveform
-        for (length = attack + decay + sustain + release + delay | 0; i < length; b[i++] = s) {
-            if (!(++c % (bitCrush * 100 | 0))) // bit crush
-             {
-                s = shape ? shape > 1 ? shape > 2 ? shape > 3 ? // wave shape
-                    Math.sin((t % PI2) ** 3) : // 4 noise
-                    Math.max(Math.min(Math.tan(t), 1), -1) : // 3 tan
-                    1 - (2 * t / PI2 % 2 + 2) % 2 : // 2 saw
-                    1 - 4 * Math.abs(Math.round(t / PI2) - t / PI2) : // 1 triangle
-                    Math.sin(t); // 0 sin
-                s = (repeatTime ?
-                    1 - tremolo + tremolo * Math.sin(PI2 * i / repeatTime) // tremolo
-                    : 1) *
-                    this.sign(s) * (Math.abs(s) ** shapeCurve) * // curve 0=square, 2=pointy
-                    volume * zzfxV * ( // envelope
-                i < attack ? i / attack : // attack
-                    i < attack + decay ? // decay
-                        1 - ((i - attack) / decay) * (1 - sustainVolume) : // decay falloff
-                        i < attack + decay + sustain ? // sustain
-                            sustainVolume : // sustain volume
-                            i < length - delay ? // release
-                                (length - i - delay) / release * // release falloff
-                                    sustainVolume : // release volume
-                                0); // post release
-                s = delay ? s / 2 + (delay > i ? 0 : // delay
-                    (i < length - delay ? 1 : (length - i) / delay) * // release delay 
-                        b[i - delay | 0] / 2) : s; // sample delay
-            }
-            f = (frequency += slide += deltaSlide) * // frequency
-                Math.cos(modulation * tm++); // modulation
-            t += f - f * noise * (1 - (Math.sin(i) + 1) * 1e9 % 2); // noise
-            if (j && ++j > pitchJumpTime) // pitch jump
-             {
-                frequency += pitchJump; // apply pitch jump
-                startFrequency += pitchJump; // also apply to start
-                j = 0; // reset pitch jump time
-            }
-            if (repeatTime && !(++r % repeatTime)) // repeat
-             {
-                frequency = startFrequency; // reset frequency
-                slide = startSlide; // reset slide
-                j = j || 1; // reset pitch jump time
-            }
-        }
-        return b;
-    }
-}
+const zzfxm = require("libs/zzfxm");
+// https://keithclark.github.io/ZzFXM/
+// Song data for "I am back". Original song by Sky.
+const song = zzfxm.default(...[[[1.8, 0, 72, , , .2, , 4, -2, 6, 50, .15, , 6], [, 0, 655, , , .09, 3, 1.65, , , , , .02, 3.8, -.1, , .2], [1.2, 0, 23, , , .2, 3, 4, , , 3, .9, .05], [1.5, 0, 740, , , .15, 2, .2, -.1, -.15, 9, .02, , .1, .12, , .06]], [[[3, -1, 13, 13, 13, 8, 13, , , , , , , , , , , , 11, 11, 11, 6, 11, , , , , , , , , , , , 10, 10, 10, 6, 10, , , , , , , , , 6, 8, 10, 8, 8, 8, 5, 13, , 8, 8, 8, 5, 13, , , , , ,], [, 1, 25, , 25, , , , , , , , , , , , , 25, 25, , 25, , , , , , , 25, , , 25, , 25, 25, 25, , 25, , , , , , , , , , , 25, 25, 25, 25, , 25, , , , , , , , , , , , , ,], [2, -1, 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, , 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, , , 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, , 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, ,]], [[3, -1, 13, 13, 13, 8, 13, , , , , , , , , , , , 11, 11, 11, 6, 11, , , , , , , , , , , , 10, 10, 10, 6, 10, , , , , , , , , 6, 8, 10, 8, 8, 8, 5, 13, , 8, 8, 8, 5, 13, 8, 8, 8, 5, 13], [2, -1, 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, 27, 11, , 23, , 11, 11, 23, 11, , 11, 23, 11, 11, 11, 23, 22, 18, , 30, , 18, 18, 30, 18, , 18, 30, 18, 18, 18, 30, 22, 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, ,], [, 1, 25, , 25, , , , , , , , , , , , , 25, 25, , 25, , , , , , , , , , , , , , 25, , 25, , , , , , , , , , , 25, 25, 25, 25, , 25, , , , , , , , , , , , , ,], [1, 1, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , 13, 13, 13, 13, 13, 13, 13, 13]], [[3, -1, 13, 13, 13, 8, 13, , 13, 15, 17, 17, 15, 13, 20, 20, 18, 17, 18, , , , 17, , 15, , , , 17, , 18, , 22, 22, 22, , 18, , , , 25, 25, 25, , 22, , , 18, 20, 22, 20, , , , , , , , , , , , , , , ,], [2, -1, 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, 27, 11, , 23, , 11, 11, 23, 11, , 11, 23, 11, 11, 11, 23, 22, 18, , 30, , 18, 18, 30, 18, , 18, 30, 18, 18, 18, 30, 22, 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, ,], [, 1, 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , ,], [1, 1, , , , , 13, , , , , , , , 13, , , , , , , , 13, , , , , , , , 13, , 13, , , , , , 13, , , , , , , , 13, , , , , , , , 13, , , , , , , , 13, 13, , 13]], [[3, -1, 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, , 11, , 23, , 11, 11, 23, 11, , 11, 23, 11, 11, 11, 23, , 10, , 22, , 10, 10, 22, 10, , 10, 22, 10, 10, 6, 8, 10, 20, 25, 20, 20, 25, 20, , 20, 25, 20, 20, 20, 25, , 20, ,], [2, -1, 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, , 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, , 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, , 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, ,], [, 1, 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , ,], [1, 1, , , , , 13, , , , , , , , 13, , , , , , , , 13, , , , , , , , 13, , 13, , , , , , 13, , , , , , , , 13, , , , , , , , 13, , , , , , , , 13, 13, , 13]], [[3, -1, 13, , , , , , 8, , 17, 15, 13, , 17, 15, 13, , 15, , , , 10, 13, 15, 10, 13, 15, 10, 13, 15, 10, 13, 15, 12, , , , , , 8, 15, , , , , 17, 15, 13, 8, 13, , , , , , 10, 8, , 20, 20, 20, 20, 20, 20, 20], [2, -1, 13, , 25, , 13, 13, 25, 13, , 13, 25, 13, 13, 13, 25, , 15, , 27, , 15, 15, 27, 15, , 15, 27, 15, 15, 15, 27, 32, 20, , 32, , 20, 20, 32, 20, , 20, 32, 20, 20, 20, 32, , 13, , 25, , 13, 13, 25, 20, , 20, 32, 20, 20, 20, 32, ,], [, 1, 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , ,], [1, 1, , , , , 13, , , , , , , , 13, , , , , , , , 13, , , , , , , , 13, , 13, , , , , , 13, , , , , , , , 13, , , , , , , , 13, , , , , , , , 13, 13, , 13]], [[3, -1, 13, , , , , , 8, , 17, , , , 18, 17, 15, , 18, , , , 13, , , , 10, , , , 6, , , , 8, 12, 15, 12, 20, , 8, 12, 15, 12, 20, , 22, 20, 15, , 13, , , , , , 10, , 8, , , , , 8, 20, 8], [2, -1, 13, , 25, 25, 13, , 25, 25, 13, , 25, 25, 13, , 25, 25, 15, , 27, 27, 15, , 27, 27, 15, , 27, 27, 15, , 27, 27, 20, , 32, 32, 20, , 32, 32, 20, , 32, 32, 20, , 32, 32, 13, , 25, 25, 13, , 25, 25, 20, , 32, 32, 20, , 32, 34], [, 1, 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , ,], [1, 1, , , , , 13, , , , , , , , 13, , , , , , , , 13, , , , , , , , 13, , 13, , , , , , 13, , , , , , , , 13, , , , , , , , 13, , , , , , , , 13, 13, , 13]], [[3, -1, 13, , , , , , 8, , 17, , , , 18, 17, 15, , 18, , , , 13, , , , 10, , , , 6, , , , 8, 12, 15, 12, 20, , 8, 12, 15, 12, 20, , 22, 20, 15, , 13, , , , , , 10, , 8, , , , , 8, 20, 8], [, 1, 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , ,], [1, 1, , , , , 13, , , , , , , , 13, , , , , , , , 13, , , , , , , , 13, , 13, , , , , , 13, , , , , , , , 13, , , , , , , , 13, , , , , , , , 13, 13, , 13]], [[, 1, 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , , , 25, 25, , 25, 25, , , , , , 25, , , , 25, , , 25, , , , 25, 25, 25, 25, 25], [1, 1, , , , , 13, , , , , , , , 13, , , , , , , , 13, , , , , , , , 13, , 13, , , , , , 13, , , , , , , , 13, , , , , , , , 13, 13, , 13, , 13, 13, 13, 13, 13, 13, 13]]], [0, 1, 2, 2, 3, 3, 2, 2, 4, 4, 5, 6, 6, 7, 2, 2, 3]]);
 /**
  * Audio Generator Demo (TypeScript version)
  *  This is a demo showing how one can generate and play audio samples from TypeScript.
@@ -93,25 +22,23 @@ class AudioGeneratorDemo extends godot_1.Node {
     constructor() {
         super(...arguments);
         this.cached_frame = new godot_1.Vector2();
-        this.buffer_index = 0;
+        this.frame_index = 0;
     }
     //BUG dont know why this crashs v8
     // private xxx = zzfx.generate();
-    _init() {
-        console.log("generator init");
-    }
     _ready() {
-        console.log("generate samples buffer");
-        this.buffer = zzfx.generate(1.74, .05, 598, .09, .26, .41, 0, .24, 0, 0, -46, .07, .17, 0, 0, 0, .09, .42, .28, .06);
-        this.player.stream.mix_rate = zzfxR;
+        console.log("song samples", song[0].length);
+        // this.buffer = zzfx.generate(1.74,.05,598,.09,.26,.41,0,.24,0,0,-46,.07,.17,0,0,0,.09,.42,.28,.06);
+        this.player.stream.mix_rate = zzfxm.zzfxR;
         this.player.play();
         this.playback = this.player.get_stream_playback();
     }
     _process(delta) {
         let to_fill = this.playback.get_frames_available();
         while (to_fill > 0) {
-            const v = this.buffer[this.buffer_index++ % this.buffer.length];
-            this.cached_frame.x = this.cached_frame.y = v;
+            this.frame_index = (this.frame_index + 1) % song[0].length;
+            this.cached_frame.x = song[0][this.frame_index];
+            this.cached_frame.y = song[1][this.frame_index];
             this.playback.push_frame(this.cached_frame); // Audio frames are stereo.
             --to_fill;
         }
---
+++ javascripts/libs/zzfxm.js
@@ -0,0 +1,145 @@
+"use strict";
+
+/**
+ * ZzFX Music Renderer v2.0.3 by Keith Clark and Frank Force
+ */
+
+/**
+ * @typedef Channel
+ * @type {Array.<Number>}
+ * @property {Number} 0 - Channel instrument
+ * @property {Number} 1 - Channel panning (-1 to +1)
+ * @property {Number} 2 - Note
+ */
+
+/**
+ * @typedef Pattern
+ * @type {Array.<Channel>}
+ */
+
+/**
+ * @typedef Instrument
+ * @type {Array.<Number>} ZzFX sound parameters
+ */
+
+/**
+ * Generate a song
+ *
+ * @param {Array.<Instrument>} instruments - Array of ZzFX sound paramaters.
+ * @param {Array.<Pattern>} patterns - Array of pattern data.
+ * @param {Array.<Number>} sequence - Array of pattern indexes.
+ * @param {Number} [speed=125] - Playback speed of the song (in BPM).
+ * @returns {Array.<Array.<Number>>} Left and right channel sample data.
+ */
+
+// zzfxG() - the sound generator -- returns an array of sample data
+let zzfxG=(q=1,k=.05,c=220,e=0,t=0,u=.1,r=0,F=1,v=0,z=0,w=0,A=0,l=0,B=0,x=0,G=0,d=0,y=1,m=0,C=0)=>{let b=2*Math.PI,H=v*=500*b/zzfxR**2,I=(0<x?1:-1)*b/4,D=c*=(1+2*k*Math.random()-k)*b/zzfxR,Z=[],g=0,E=0,a=0,n=1,J=0,K=0,f=0,p,h;e=99+zzfxR*e;m*=zzfxR;t*=zzfxR;u*=zzfxR;d*=zzfxR;z*=500*b/zzfxR**3;x*=b/zzfxR;w*=b/zzfxR;A*=zzfxR;l=zzfxR*l|0;for(h=e+m+t+u+d|0;a<h;Z[a++]=f)++K%(100*G|0)||(f=r?1<r?2<r?3<r?Math.sin((g%b)**3):Math.max(Math.min(Math.tan(g),1),-1):1-(2*g/b%2+2)%2:1-4*Math.abs(Math.round(g/b)-g/b):Math.sin(g),f=(l?1-C+C*Math.sin(2*Math.PI*a/l):1)*(0<f?1:-1)*Math.abs(f)**F*q*zzfxV*(a<e?a/e:a<e+m?1-(a-e)/m*(1-y):a<e+m+t?y:a<h-d?(h-a-d)/u*y:0),f=d?f/2+(d>a?0:(a<h-d?1:(h-a)/d)*Z[a-d|0]/2):f),p=(c+=v+=z)*Math.sin(E*x-I),g+=p-p*B*(1-1E9*(Math.sin(a)+1)%2),E+=p-p*B*(1-1E9*(Math.sin(a)**2+1)%2),n&&++n>A&&(c+=w,D+=w,n=0),!l||++J%l||(c=D,v=H,n=n||1);return Z}
+
+// zzfxV - global volume
+let zzfxV=.3
+
+// zzfxR - global sample rate
+let zzfxR=44100
+
+let zzfxM = (instruments, patterns, sequence, BPM = 125) => {
+  let instrumentParameters;
+  let i;
+  let j;
+  let k;
+  let note;
+  let sample;
+  let patternChannel;
+  let notFirstBeat;
+  let stop;
+  let instrument;
+  let pitch;
+  let attenuation;
+  let outSampleOffset;
+  let isSequenceEnd;
+  let sampleOffset = 0;
+  let nextSampleOffset;
+  let sampleBuffer = [];
+  let leftChannelBuffer = [];
+  let rightChannelBuffer = [];
+  let channelIndex = 0;
+  let panning = 0;
+  let hasMore = 1;
+  let sampleCache = {};
+  let beatLength = zzfxR / BPM * 60 >> 2;
+
+  // for each channel in order until there are no more
+  for(; hasMore; channelIndex++) {
+
+    // reset current values
+    sampleBuffer = [hasMore = notFirstBeat = pitch = outSampleOffset = 0];
+
+    // for each pattern in sequence
+    sequence.map((patternIndex, sequenceIndex) => {
+      // get pattern for current channel, use empty 1 note pattern if none found
+      patternChannel = patterns[patternIndex][channelIndex] || [0, 0, 0];
+
+      // check if there are more channels
+      hasMore |= !!patterns[patternIndex][channelIndex];
+
+      // get next offset, use the length of first channel
+      nextSampleOffset = outSampleOffset + (patterns[patternIndex][0].length - 2 - !notFirstBeat) * beatLength;
+      // for each beat in pattern, plus one extra if end of sequence
+      isSequenceEnd = sequenceIndex == sequence.length - 1;
+      for (i = 2, k = outSampleOffset; i < patternChannel.length + isSequenceEnd; notFirstBeat = ++i) {
+
+        // <channel-note>
+        note = patternChannel[i];
+
+        // stop if end, different instrument or new note
+        stop = i == patternChannel.length + isSequenceEnd - 1 && isSequenceEnd ||
+            instrument != (patternChannel[0] || 0) | note | 0;
+
+        // fill buffer with samples for previous beat, most cpu intensive part
+        for (j = 0; j < beatLength && notFirstBeat;
+
+            // fade off attenuation at end of beat if stopping note, prevents clicking
+            j++ > beatLength - 99 && stop ? attenuation += (attenuation < 1) / 99 : 0
+        ) {
+          // copy sample to stereo buffers with panning
+          sample = (1 - attenuation) * sampleBuffer[sampleOffset++] / 2 || 0;
+          leftChannelBuffer[k] = (leftChannelBuffer[k] || 0) - sample * panning + sample;
+          rightChannelBuffer[k] = (rightChannelBuffer[k++] || 0) + sample * panning + sample;
+        }
+
+        // set up for next note
+        if (note) {
+          // set attenuation
+          attenuation = note % 1;
+          panning = patternChannel[1] || 0;
+          if (note |= 0) {
+            // get cached sample
+            sampleBuffer = sampleCache[
+              [
+                instrument = patternChannel[sampleOffset = 0] || 0,
+                note
+              ]
+            ] = sampleCache[[instrument, note]] || (
+                // add sample to cache
+                instrumentParameters = [...instruments[instrument]],
+                instrumentParameters[2] *= 2 ** ((note - 12) / 12),
+
+                // allow negative values to stop notes
+                note > 0 ? zzfxG(...instrumentParameters) : []
+            );
+          }
+        }
+      }
+
+      // update the sample offset
+      outSampleOffset = nextSampleOffset;
+    });
+  }
+
+  return [leftChannelBuffer, rightChannelBuffer];
+}
+
+exports.zzfxG = zzfxG;
+exports.zzfxV = zzfxV;
+exports.zzfxR = zzfxR;
+exports.zzfxM = zzfxM;
+exports.default = zzfxM;
