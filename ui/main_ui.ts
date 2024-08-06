068c3645f3df51c8a1ab5f99001a2f2087be1d49
ialex32x <lin_032@hotmail.com>
2024-08-06 19:12:48 +0800

    test: validated built/utility function call

--- ui/main_ui.ts
+++ ui/main_ui.ts
@@ -1,4 +1,4 @@
-import { Label, Node2D, NodePath, Variant } from "godot";
+import { Label, Node2D, NodePath, randf, Variant, Vector2 } from "godot";
 import * as jsb from "godot-jsb";
 import TestNode from "../test_button";
 import { export_, seconds } from "../jsb/jsb.core";
@@ -15,20 +15,28 @@ export default class MainUI extends Node2D {
 			button.test_signal.connect(jsb.callable(this, this.on_test), 0);
 		}
 
-		(<Label>this.get_node("Control/Label")).text = "wait for 3 seconds with await";
-		await seconds(3);
-		(<Label>this.get_node("Control/Label")).text = "3 seconds passed";
+        let ticks = 5;
+        for (let i = ticks; i > 0; i--) {
+            (<Label>this.get_node("Control/Label")).text = `wait for ${i} seconds with await`;
+            await seconds(1);
+        }
+        (<Label>this.get_node("Control/Label")).text = `${ticks} seconds passed`;
 	}
 
 	private on_test() {
-		console.log("test signal emitted");
+        console.log("test signal emitted",
+            "randf:", randf(), // test utility function call  
+            "Vector2.from_angle:", Vector2.from_angle(10), // test builtin static function
+            "Vector2.length:", new Vector2(1, 1).length(), // test builtin instance method
+            ""
+        );
 		const label = <Label>this.get_node(new NodePath("Control/Label"));
 		if (label) {
 			label.text = `hello ${this._pc++}`;
 		} else {
 			console.warn("Control/Label not found");
 		}
-
+        
 		console.trace("trace test");
 		console.debug("debug test");
 		console.info("info test");
