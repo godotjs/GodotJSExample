2ff3df9a7a57665d2b77496de0ad847b36c05767
ialex32x <lin_032@hotmail.com>
2024-08-07 16:21:08 +0800

    chore: code format

--- ui/main_ui.ts
+++ ui/main_ui.ts
@@ -4,16 +4,16 @@ import TestNode from "../test_button";
 import { export_, seconds } from "../jsb/jsb.core";
 
 export default class MainUI extends Node2D {
-	private _pc = 0;
+    private _pc = 0;
 
-	@export_(Variant.Type.TYPE_FLOAT)
-	speed = 200
+    @export_(Variant.Type.TYPE_FLOAT)
+    speed = 200
 
-	async _ready() {
-		const button = <TestNode>this.get_node(new NodePath("Control/Button"));
-		if (button) {
-			button.test_signal.connect(jsb.callable(this, this.on_test), 0);
-		}
+    async _ready() {
+        const button = <TestNode>this.get_node(new NodePath("Control/Button"));
+        if (button) {
+            button.test_signal.connect(jsb.callable(this, this.on_test), 0);
+        }
 
         let ticks = 5;
         for (let i = ticks; i > 0; i--) {
@@ -21,27 +21,27 @@ export default class MainUI extends Node2D {
             await seconds(1);
         }
         (<Label>this.get_node("Control/Label")).text = `${ticks} seconds passed`;
-	}
+    }
 
-	private on_test() {
+    private on_test() {
         console.log("test signal emitted",
             "randf:", randf(), // test utility function call  
             "Vector2.from_angle:", Vector2.from_angle(10), // test builtin static function
             "Vector2.length:", new Vector2(1, 1).length(), // test builtin instance method
             ""
         );
-		const label = <Label>this.get_node(new NodePath("Control/Label"));
-		if (label) {
-			label.text = `hello ${this._pc++}`;
-		} else {
-			console.warn("Control/Label not found");
-		}
-        
-		console.trace("trace test");
-		console.debug("debug test");
-		console.info("info test");
-		console.log("log test");
-		console.warn("warn test");
-		console.error("error test");
-	}
+        const label = <Label>this.get_node(new NodePath("Control/Label"));
+        if (label) {
+            label.text = `hello ${this._pc++}`;
+        } else {
+            console.warn("Control/Label not found");
+        }
+
+        console.trace("trace test");
+        console.debug("debug test");
+        console.info("info test");
+        console.log("log test");
+        console.warn("warn test");
+        console.error("error test");
+    }
 }
